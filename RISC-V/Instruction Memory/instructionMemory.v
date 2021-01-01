77c88d50c77bbc57a58964a08282a8ad88686ed5
Reza Mansouri <mrmansourireza@gmail.com>
2021-01-01 23:01:05 +0330

    Write instructonMemory Module with TestBench

---
+++ RISC-V/Instruction Memory/instructionMemory.v
@@ -0,0 +1,11 @@
+//Reza Mansouri 972023033
+module instructionMemory(addressInput, instructionOutput);
+  input [47:0] addressInput;
+  //As The Instructor has stated, i should have 48 bit addresses as input
+  output [31:0] instructionOutput;
+  //Risc-V instructions are each 31 bit long
+  reg [47:0] memory [31:0];
+  //i define a verilog memory array which has 48 bit addresses and 32 bit data
+  assign instructionOutput = RAM[addressInput];
+  //i assign the previously defined output the the Data on ram ath the address input
+endmodule
---
+++ RISC-V/Instruction Memory/instructionMemoryTestBench.v
@@ -0,0 +1,16 @@
+//Reza Mansouri 972023033
+//i include the instruction memory module
+`include "instruction_memory.v
+module instructionMemoryTestBench;
+  //i declare an 48 bit array
+  reg address[47:0];
+  //i assign a 48 bit HEX valye to the address
+  //in 48'h12345678abcd -> 48 is the size of the address in bits
+  //                       'h means the notaition for this address is in HEX
+  //                       12345678abcd is 12 HEX digits (12*4 = 48)
+  assign address = 48'h12345678abcd;
+  //i declare an 32 bit array that im going to fetch an instruction to
+  reg instruction[31:0];
+  //i declare an instructionMemory instance
+  instructionMemory instructionRam(address, instruction);
+endmodule
